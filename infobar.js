82186fcd4e0d561ecd10d1a13188fbd040d9e6fd
kucaahbe <kucaahbe@ukr.net>
2013-02-05 19:57:19 +0200

    fixed bug on first with infobar appearing:
    on first try to send message to infobar's port,
    it could be not present at that time(and in most cases it is),
    because adding listener function is wrapped by chrome.tabs.getCurrent
    so we are adding listener firstly, and in callback getting current tab
    id and saving it in window object

--- infobar.js
+++ infobar.js
@@ -25,14 +25,22 @@ function translateTargetWord(word) {
   xhr.send();
 };
 
-chrome.tabs.getCurrent(function (tab) {
-  console.log("tab id:",tab.id);
+function reseiveMessageFromPort(port_name,tab_id,data) {
+  if (port_name == "infobar-"+tab_id) {
+    updateTargetWord(data.selection);
+  }
+};
 
-  chrome.extension.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(data) {
-      if (port.name == "infobar-"+tab.id) {
-        updateTargetWord(data.selection);
-      }
-    });
+chrome.extension.onConnect.addListener(function(port) {
+  port.onMessage.addListener(function(data) {
+    if (window.tab_id) {
+      reseiveMessageFromPort(port.name,window.tab_id,data);
+    } else {
+      chrome.tabs.getCurrent(function (tab) {
+        console.log("tab id:",tab.id);
+        window.tab_id = tab.id;
+        reseiveMessageFromPort(port.name,window.tab_id,data);
+      });
+    }
   });
 });
