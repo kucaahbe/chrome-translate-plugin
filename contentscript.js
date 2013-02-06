193c006d19c7f2be282fef7cd96701c0a124081b
kucaahbe <kucaahbe@ukr.net>
2013-02-07 00:11:56 +0200

    do not send empty selection

--- contentscript.js
+++ contentscript.js
@@ -3,12 +3,23 @@ document.addEventListener("keyup",sendSelectionToTranslator);
 
 var selection = null;
 
+function makesSenseToSendSelection() {
+  if (typeof(selection) != "string") {
+    return false;
+  }
+
+  // removes blanks from start and end
+  selection = selection.trim();
+  // so only non-empty selection should go to translator
+  return (selection.length > 0);
+};
+
 function sendSelectionToTranslator() {
 
   var current_selection = window.getSelection().toString();
   selection = (selection == current_selection ? null : current_selection);
 
-  if (typeof(selection) == "string" && selection.length > 0) {
+  if (makesSenseToSendSelection()) {
     console.debug("selection: \"%s\" sent to translator", selection);
 
     // send data to event page
